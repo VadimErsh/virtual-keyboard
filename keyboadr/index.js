89dba7c36e47c1b77fccbe0a228fe40274608ed5
VadimErsh <vadimersh2022@gmail.com>
2023-04-29 05:19:33 +0300

    feat: functional caps lock button

--- keyboadr/index.js
+++ keyboadr/index.js
@@ -47,7 +47,6 @@ const KeyBoardBody = document.getElementById('keyboard')
     const KeyCypOne = document.createElement('div')
     KeyCypOne.className = `key ${CodeClassOne[i]}`
     KeyCypOne.innerText = String.fromCharCode(CharLineOne[i])
-
     if (i === 13) {
       KeyCypOne.innerText = 'Backspace'
     }
@@ -61,9 +60,10 @@ const KeyBoardBody = document.getElementById('keyboard')
 
   for (let i = 0 ; i < CharLineTwo.length ; i++) {
     const KeyCypTwo = document.createElement('div')
-    KeyCypTwo.className = `key ${CodeClassTwo[i]}`
+    const classAtribut = CodeClassTwo[i].slice(0,3)
+    KeyCypTwo.className = `key ${CodeClassTwo[i]} ${classAtribut}`
     KeyCypTwo.innerText = String.fromCharCode(CharLineTwo[i]).toLocaleLowerCase()
-
+    
     if (i === 0) {
       KeyCypTwo.innerText = 'Tab'
     }
@@ -76,8 +76,15 @@ const KeyBoardBody = document.getElementById('keyboard')
       KeyCypTwo.innerText = "\\"
     }
 
-    KeyLineTwo.append(KeyCypTwo)
+    if (i === 12) {
+      KeyCypTwo.innerText = "]"
+    }
 
+    if (i === 11) {
+      KeyCypTwo.innerText = "["
+    }
+
+    KeyLineTwo.append(KeyCypTwo)
   }
 
   const KeyLineThree = document.createElement('div')
@@ -85,7 +92,8 @@ const KeyBoardBody = document.getElementById('keyboard')
 
   for (let i = 0 ; i < CharLineThree.length ; i++) {
     const KeyCypThree = document.createElement('div')
-    KeyCypThree.className = `key ${CodeClassThree[i]}`
+    const classAtribut = CodeClassThree[i].slice(0,3)
+    KeyCypThree.className = `key ${CodeClassThree[i]} ${classAtribut}`
     KeyCypThree.innerText = String.fromCharCode(CharLineThree[i]).toLocaleLowerCase()
 
     if (i === 0) {
@@ -114,7 +122,8 @@ const KeyBoardBody = document.getElementById('keyboard')
 
   for (let i = 0 ; i < CharLineFour.length ; i++) {
     const KeyCypFour = document.createElement('div')
-    KeyCypFour.className = `key ${CodeClassFour[i]}`
+    const classAtribut = CodeClassFour[i].slice(0,3)
+    KeyCypFour.className = `key ${CodeClassFour[i]} ${classAtribut}`
     KeyCypFour.innerText = String.fromCharCode(CharLineFour[i]).toLocaleLowerCase()
 
     if (i === 0 || i === 12) {
@@ -146,7 +155,8 @@ const KeyBoardBody = document.getElementById('keyboard')
 
   for (let i = 0 ; i < CharLineFive.length ; i++) {
     const KeyCypFive = document.createElement('div')
-    KeyCypFive.className = `key ${CodeClassFive[i]}`
+    const classAtribut = CodeClassFive[i].slice(0,3)
+    KeyCypFive.className = `key ${CodeClassFive[i]} ${classAtribut}`
     KeyCypFive.innerText = String.fromCharCode(CharLineFive[i])
 
     if (i === 0 || i === 8) {
@@ -158,7 +168,7 @@ const KeyBoardBody = document.getElementById('keyboard')
     }
 
     if (i === 3) {
-      KeyCypFive.innerText = ''
+      KeyCypFive.innerText = ' '
     }
 
     if (i === 2 || i === 4) {
@@ -200,28 +210,87 @@ ArrayKeys.forEach((el) => {
   let result = ''
 
   el.addEventListener('click' , (event) => {
-    result += event.target.innerHTML
-    TextArea.textContent += result
-    result=''
+    if (event.target.innerHTML === "Caps Lock" ||
+        event.target.innerHTML === "Ctrl" ||
+        event.target.innerHTML === "Alt" ||
+        event.target.innerHTML === "Shift" ||
+        event.target.innerHTML === "Backspace" ||
+        event.target.innerHTML === "Del"  ||
+        event.target.innerHTML === "Enter" ||
+        event.target.innerHTML === "Tab" ||
+        event.target.innerHTML === "Win") {
+
+        result += ''
+    } else {
+        result += event.target.innerHTML
+        TextArea.textContent += result
+        result=''
+    }
   })
 
-  el.addEventListener('mousedown' ,() => {el.classList.add('active')}) 
-  el.addEventListener('mouseup' ,() => {el.classList.remove('active')})  
+  el.addEventListener('mousedown' ,() => {el.classList.add('active')})
+  el.addEventListener('mouseup' ,() => {el.classList.remove('active')})
 
 })
 
 document.addEventListener('keydown' , (event) => {
 
- 
-  for (let i = 0; i < ArrayKeys.length; i++) { 
-  if (ArrayKeys[i].classList[1] === event.code) { console.log(ArrayKeys[i].textContent)
+  for (let i = 0; i < ArrayKeys.length; i++) {
+
+  if (ArrayKeys[i].classList[1] === event.code) {
     ArrayKeys[i].classList.add('active')
     document.addEventListener('keyup', () => {ArrayKeys[i].classList.remove('active')})
-
-    TextArea.textContent += ArrayKeys[i].textContent
+    if (ArrayKeys[i].classList[1] === "CapsLock" ||
+    ArrayKeys[i].classList[1] === "ControlLeft" ||
+    ArrayKeys[i].classList[1] === "ControlRight" ||
+    ArrayKeys[i].classList[1] === "AltRight" ||
+    ArrayKeys[i].classList[1] === "AltLeft" ||
+    ArrayKeys[i].classList[1] === "ShiftLeft" ||
+    ArrayKeys[i].classList[1] === "ShiftRight" ||
+    ArrayKeys[i].classList[1] === "Backspace" ||
+    ArrayKeys[i].classList[1] === "Delete"  ||
+    ArrayKeys[i].classList[1] === "MetaLeft"  ||
+    ArrayKeys[i].classList[1] === "Enter" ||
+    ArrayKeys[i].classList[1] === "Tab" ||
+    ArrayKeys[i].classList[1] === "Win"){
+      TextArea.textContent += ''
+    } else {
+      TextArea.textContent += ArrayKeys[i].textContent
+    }
   }
  }
 })
+
+const lettersKey = document.querySelectorAll('.Key')
+const ArrayLettersKey = Array.from(lettersKey)
+
+
+document.addEventListener('keyup', (event) => {
+
+ if (event.key === "CapsLock") {
+      document.querySelector('.CapsLock').classList.toggle('activecaps')
+      ArrayLettersKey.forEach((el) => {
+      el.textContent = el.textContent.toLocaleUpperCase()
+
+ if (!document.querySelector('.CapsLock').classList.contains('activecaps')) {
+      el.textContent = el.textContent.toLocaleLowerCase()
+     }
+  })
+ }
+})
+
+document.querySelector('.CapsLock').addEventListener('click', () => {
+  document.querySelector('.CapsLock').classList.toggle('activecaps')
+  if (document.querySelector('.CapsLock').classList.contains('activecaps')) {
+       ArrayLettersKey.forEach((el) => {
+       el.textContent = el.textContent.toLocaleUpperCase()
+      })
+    } else {
+      ArrayLettersKey.forEach((el) => {
+        el.textContent = el.textContent.toLocaleLowerCase()
+      })
+    }
+ })
 
 
 
